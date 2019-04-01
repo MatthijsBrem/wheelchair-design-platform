d81d0b6ebf70517938030f9b3ac348208b7fe35a
tjapko <tjapko94@hotmail.com>
2019-04-01 12:20:11 +0200

    merge fix?

--- aupyom-master/aupyom/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sound import Sound
-from .sampler import Sampler
-
-from .version import version as __version__
--- aupyom-master/ci/download_data.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pushd aupyom/example_data
-    for f in *.wav; do
-        echo "Downloading $f..."
-        wget "https://media.githubusercontent.com/media/pierre-rouanet/aupyom/$TRAVIS_BRANCH/aupyom/example_data/$f" -O "$f"
-    done
-popd
