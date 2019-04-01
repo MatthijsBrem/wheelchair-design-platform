6a72d5bd0ad42bf92c2f65e3ceffad65c31fbc3d
Matthijs Bremmer <matthijsbremmer@gmail.com>
2019-04-01 10:02:26 +0200

    Controller pure data updated

--- DiscoChair/sendToPD.py
+++ DiscoChair/sendToPD.py
@@ -3,27 +3,37 @@ import time
 
 
 def send2Pd(message=''):
-    os.system("echo '" + message + "' | pdsend 3000")
+    os.system("echo '" + message + "' | pdsend 1000")
 
 
-def audioOn():
+def audio_on():
     message = '0 1'
     send2Pd(message)
 
-def audioOff():
-    message = '0 0'
+def audio_reverse():
+    message = '1 1'
     send2Pd(message)
 
-
-
-def setVolume():
-    vol = 80
-    message = '1' + str(vol) + ';'
+def audio_off():
+    message = '2 1'
     send2Pd(message)
 
+def hipassf():
+    hi = 100
+    message = '3' + str(hi) + ';'
+    send2Pd(message)
 
-while True:
-    audioOn()
-    time.sleep(3)
-    audioOff()
-    time.sleep(3)
+def lowpassf():
+    low = 10000
+    message = '4' + str(low) + ';'
+    send2Pd(message)
+
+def delay():
+    ddelay = 0.5
+    message = '5' + str(ddelay) + ';'
+    send2Pd(message)
+
+def pitch():
+    tempo = 0.003
+    message = '6' + str(tempo) + ';'
+    send2Pd(message)
---
+++ PureData/send2PD.py
@@ -0,0 +1,3 @@
+import os
+
+def send2Pd (message)
