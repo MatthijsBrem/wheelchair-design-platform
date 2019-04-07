c66e1ef67989e064556df4f681891eaadcf6b4a9
tjapko <tjapko94@hotmail.com>
2019-04-07 17:36:28 +0200

    debug line

--- wheelchair/reading_from_terminal.py
+++ wheelchair/reading_from_terminal.py
@@ -135,7 +135,7 @@ def serial_Reader():
                 print("did not get 4 values")
         elif(property_id == GestureID):
             print("the gesture is as follows: ")
-            print (GestureID)
+            print (values)
             if values == 4:
                 reverse_str = "2 1 ;"
                 s.send(reverse_str.encode('utf-8'))
