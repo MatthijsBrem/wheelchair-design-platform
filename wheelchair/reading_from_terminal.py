2dbe6151954d4fef19e665f2b897fec3615393c0
Matthijs Bremmer <matthijsbremmer@gmail.com>
2019-04-07 12:01:34 +0200

    read

---
+++ wheelchair/reading_from_terminal.py
@@ -0,0 +1,17 @@
+# Import required library
+import os                       # To access environment variables
+from dotenv import load_dotenv  # To load environment variables from .env file
+import serial                   # To connect via the serial port
+import time                     # To sleep for a few seconds
+
+# The thing ID and access token
+load_dotenv()
+
+# Start reading the serial port
+ser = serial.Serial(
+    port = os.environ['SERIAL'],
+    baudrate = 9600,
+    write_timeout = 0)
+
+
+    ser.read()
