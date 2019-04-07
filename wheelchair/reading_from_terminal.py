09278b3e631051d8c7c1c96a5dbf66639e0cb5c0
tjapko <tjapko94@hotmail.com>
2019-04-07 14:33:20 +0200

    added the reverse

--- wheelchair/reading_from_terminal.py
+++ wheelchair/reading_from_terminal.py
@@ -44,6 +44,9 @@ def translatePredictionToPD(prediction):
         music_on = "0 1 ;"
         s.send(music_on.encode('utf-8'))
         print("turning the music on")
+    elif prediction == 3:
+        reverse_on = "2 1 ;"
+        s.send(reverse_on.encode('utf-4'))
 
 
 def predict(values):
